d717b1d693a7c2790749273fdcbcd6f85142508d
Tjandra Darmo <tjandra.darmo@ajaib.co.id>
2024-02-16 00:17:56 +0700

    Time: 139 ms (76.19%) | Memory: 10.6 MB (5.95%) - LeetSync

---
+++ 3262-find-polygon-with-the-largest-perimeter/find-polygon-with-the-largest-perimeter.go
@@ -0,0 +1,23 @@
+// 1. Sort the array with asc order
+// 2. Loop through nums from the last to the third items
+// 3. If currItem < prefixSum, return currItem
+
+func largestPerimeter(nums []int) int64 {
+    slices.Sort(nums)
+
+    var perimeter int64
+    for _, num := range nums {
+        perimeter += int64(num)
+    }
+
+    for i := len(nums)-1; i >= 2; i-- {
+        sumSides := perimeter - int64(nums[i])
+        if sumSides > int64(nums[i]) {
+            return perimeter
+        }
+
+        perimeter = sumSides
+    }
+
+    return -1
+}
