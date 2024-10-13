0e0952514da5c4650b2a356118964b3178fe86f9
Tjandra Darmo <tjandradarmo@protonmail.com>
2024-10-13 14:06:45 +0700

    Time: 73 ms (39.28%) | Memory: 212.7 MB (9.86%) - LeetSync

--- 1-two-sum/two-sum.rb
+++ 1-two-sum/two-sum.rb
@@ -2,13 +2,17 @@
 # @param {Integer} target
 # @return {Integer[]}
 def two_sum(nums, target)
+    cached_nums = {}
     i = 0
-    until i == nums.length() do
-        j = i + 1
-        until j == nums.length() do
-            return [i, j] if nums[i] + nums[j] == target
-            j += 1
-        end
+    while i < nums.length()
+        cached_nums[nums[i]] = i if cached_nums[nums[i]].nil?
+        i += 1
+    end
+
+    i = 0
+    while i < nums.length()
+        diff = target - nums[i]
+        return [i, cached_nums[diff]] if !cached_nums[diff].nil? && cached_nums[diff] != i
         i += 1
     end
 
